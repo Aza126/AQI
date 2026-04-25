6027dad8845797cb7ead976fecc824f7341d08f3
Aza126 <221085563+Aza126@users.noreply.github.com>
2026-04-25 17:34:07 +0700

    Update downloading file

--- src/scripts/download_models.py
+++ src/scripts/download_models.py
@@ -8,13 +8,13 @@ logger = logging.getLogger(__name__)
 
 # Thay thế bằng ID thực tế của bạn trên Google Drive
 DRIVE_FILES = {
-    "artifacts/scaler.pkl": "FILE_ID_CUA_SCALER",
-    "artifacts/training_data_scaled.parquet": "FILE_ID_CUA_PARQUET",
+    "artifacts/scaler.pkl": "1IJ8Ajxpf84BBnE1nXAYmz6H6J9H-EYib",
+    "artifacts/training_data_scaled.parquet": "1TAi6PXTDq6bxBUYlQWO-_YLJlgxhQ_h8",
     "models/rf/rf_v1.pkl": "FILE_ID_CUA_RF",
     "models/lstm/lstm_v1.h5": "FILE_ID_CUA_LSTM"
 }
 
-def download_drive_artifacts():
+def download_artifact_and_model_files():
     for file_path, drive_id in DRIVE_FILES.items():
         # Đảm bảo thư mục tồn tại
         os.makedirs(os.path.dirname(file_path), exist_ok=True)
@@ -28,4 +28,4 @@ def download_drive_artifacts():
     logger.info("All artifacts and models downloaded successfully.")
 
 if __name__ == "__main__":
-    download_drive_artifacts()
+    download_artifact_and_model_files()
